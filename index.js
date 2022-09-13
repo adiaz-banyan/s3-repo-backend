f299eaf0817357fd3f9097389c7001893ae28816
adiaz-banyan <86432508+adiaz-banyan@users.noreply.github.com>
2022-09-13 12:39:37 -0700

    handling image on server

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 8080;
-const route = require("./routes/routeOne");
+const route = require('./routes/routeOne');
 
-app.use(express.static("src"));
-
-app.use("/api", route);
+app.use(cors());
+app.use(express.static('src'));
+app.use('/api', route);
 
 app.listen(port, () => console.log(`server running on ${port}`));
--- routes/routeOne.js
+++ routes/routeOne.js
@@ -1,11 +1,25 @@
-const express = require("express");
+const express = require('express');
+const fs = require('fs');
+const util = require('util');
+const unlinkFile = util.promisify(fs.unlink);
 const route = express.Router();
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
-route.post("/s3-url-images", upload.single("image"), (req, res) => {
-  console.log(req.file);
-  res.send("Good so far...");
+const { uploadFunc, getFileStream } = require('../s3');
+
+route.post('/s3-url-images', upload.single('image'), async (req, res) => {
+  const file = req.file;
+  const description = req.body.description;
+  const result = await uploadFunc(file);
+  await unlinkFile(file.path);
+  res.send({ imagePath: `/images/${result.Key}` });
+});
+
+route.get('/images/:key', (req, res) => {
+  const key = req.params.key;
+  const readStream = getFileStream(key);
+  readStream.pipe(res);
 });
 
 module.exports = route;
